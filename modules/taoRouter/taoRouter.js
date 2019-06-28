f273bc81658f3cfcf8de44118948e7f687380641
b2blosangeles <b2blosangeles@gmail.com>
2019-06-28 14:04:57 -0700

    Update taoRouter.js

--- modules/taoRouter/taoRouter.js
+++ modules/taoRouter/taoRouter.js
@@ -5,6 +5,15 @@
 			var me = this;
 			let v = JSON.parse(JSON.stringify(env));
 			let host = req.headers.host;
+			
+			if ( me.isIp(req.headers.host)) {
+				v.site_path = req.headers.host;
+				return v;				
+			} else {
+				v.site_path = env.root_path + '/admin';
+			}
+			return v;
+			
 			if (host) {
 				if(host.match(/^node([0-9+])\_(qa|dev|prod)\.([a-z0-9]+)\.([a-z0-9]+)$/ig)) { 
 					v.site_path = v.sites_path + '/' + 'node';
