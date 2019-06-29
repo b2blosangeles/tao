3aef76f8c46a45a2c36e7bef51db07ef6c274b1c
b2blosangeles <b2blosangeles@gmail.com>
2019-06-29 01:03:55 -0700

    Update taoRouter.js

--- modules/taoRouter/taoRouter.js
+++ modules/taoRouter/taoRouter.js
@@ -28,7 +28,7 @@
 			if ((me.isIp(req.headers.host)) && (req.query['_route'])) {
 			    if (['master', 'node', 'root', 'comm'].indexOf(req.query['_route']) !== -1) {
 				v.site_path = v.sites_path + '/' + req.query['_route'];				
-			    } else  {
+			    } else if (['master', 'node', 'root', 'comm'].indexOf(req.query['_route']) === -1)  {
 			    	v.site_path = v.root_path + '/devs/' + req.query['_route'];
 			    }
 			} else {
