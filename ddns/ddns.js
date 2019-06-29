678a637e8fd62c974765df128811862c3a3beaa9
b2blosangeles <b2blosangeles@gmail.com>
2019-06-29 14:28:47 -0700

    Update ddns.js

--- ddns/ddns.js
+++ ddns/ddns.js
@@ -24,8 +24,11 @@
 			    mh = '', m;		
 			
 			/* -- for special domain */
-
+			
 		//	delete require.cache[env.root_path + '/ddns/specialDomain.json'];
+			if (!me.specialNames) {
+				me.specialNames = require(env.root_path + '/ddns/specialDomain.json');
+			}
 		//	me.specialNames = require(env.root_path + '/ddns/specialDomain.json');
 			
 			console.log(question.name + '-->->' + env.root_path + '/ddns/specialDomain.json');
