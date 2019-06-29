ff1cc550e6ce6bd537434de15000e93f2464916a
b2blosangeles <b2blosangeles@gmail.com>
2019-06-29 15:28:53 -0700

    Update ddns.js

--- _ddns/ddns.js
+++ _ddns/ddns.js
@@ -14,20 +14,26 @@
 		
 		this.sendRecord = function(req, res) {
 			let me = this, question = req.question[0];		
-			let CP = new pkg.crowdProcess();
-			/* -- for special domain */
-			me.masterdnslist = require(env.config_path + '/dns.json');
+			let CP = new pkg.crowdProcess(), _f = {};
 			
-			if (me.masterdnslist[question.name]) {
-				me.send([{ 
-					name: question.name,
-					type: 'A',
-					class: 'IN',
-					ttl: 60,
-					data: me.masterdnslist[question.name]
-				}], req, res);	
-				return true;
-			} else {
+			_f['master'] = function(cbk) {
+				me.masterdnslist = require(env.config_path + '/dns.json');
+				if (me.masterdnslist[question.name]) {
+					me.send([{ 
+						name: question.name,
+						type: 'A',
+						class: 'IN',
+						ttl: 1,
+						data: me.masterdnslist[question.name]
+					}], req, res);
+					CP.exit = 1;
+					cbk(true);
+				} else {
+					cbk(false);
+				}	
+				
+			}
+			_f['dynamic'] = function(cbk) {
 				delete require.cache[env.config_path + '/dns.json'];
 				me.dynamicdnslist = require(env.config_path + '/dns.json');
 				
@@ -36,20 +42,29 @@
 						name: question.name,
 						type: 'A',
 						class: 'IN',
-						ttl: 10,
+						ttl: 1,
 						data: me.dynamicdnslist[question.name]
-					}], req, res);	
-					return true;
+					}], req, res);
+					CP.exit = 1;
+					cbk(true);
 				} else {
-					me.send([{ 
-						name: question.name,
-						type: 'A',
-						class: 'IN',
-						ttl: 10,
-						data: null
-					}], req, res);	
+					cbk(false);
 				}
 			}
+			CP.serial(
+				_f,
+				function(data) {
+					if (!CP.exit) {
+						me.send([{ 
+							name: question.name,
+							type: 'A',
+							class: 'IN',
+							ttl: 10,
+							data: null
+						}], req, res);	
+					}
+				}, 1000
+			);
 		};	
 	};
 	module.exports = obj;
