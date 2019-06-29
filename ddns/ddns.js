fc9e997b9d94a45b228c3a1dce1cb6342ed63079
b2blosangeles <b2blosangeles@gmail.com>
2019-06-29 14:36:13 -0700

    Update ddns.js

--- ddns/ddns.js
+++ ddns/ddns.js
@@ -13,15 +13,7 @@
 		};
 		
 		this.sendRecord = function(req, res) {
-			let me = this, question = req.question[0], 
-			    patt = {
-				    ip: /^IP\_([0-9\_]+)\.service\./ig,
-				    idx:/node([0-9]+)\.service\./ig,
-				    node:/node([0-9]+)\.service\./ig,
-				    comm:/comm([0-9]+)\.service\./ig,
-				    master:/master([0-9]+)\.service\./ig
-			    },	    
-			    mh = '', m;		
+			let me = this, question = req.question[0];		
 			
 			/* -- for special domain */
 			
@@ -46,59 +38,6 @@
 					data: null
 				}], req, res);	
 			}
-			
-			/* -- for special domain end */
-			/*
-			for (var key in patt) {
-				if (patt[key].test(question.name)) {
-					mh = key;
-					break;
-				}
-			}
-			switch (mh) {
-				case 'ip': 
-					m = new RegExp(patt[mh]).exec(question.name);
-					let ip = m[1].replace(/\_/ig, '.');
-					me.send([{ 
-						name: question.name,
-						type: 'A',
-						class: 'IN',
-						ttl: 60,
-						data: ip
-					}], req, res);
-					break;
-				case 'idx': 
-				case 'node':
-					m = new RegExp(patt[mh]).exec(question.name);
-					me.sendNodeNamedIP(question.name, m[1], req, res);
-					break;
-				case 'comm':
-					m = new RegExp(patt[mh]).exec(question.name);
-					me.sendCommNamedIP(question.name, m[1], req, res);
-					break;
-				case 'master':	
-					m = new RegExp(patt[mh]).exec(question.name);
-					me.sendMasterNamedIP(question.name, m[1], req, res);
-					break;	
-				case 'www': 
-					me.send([{ 
-						name: question.name,
-						type: 'A',
-						class: 'IN',
-						ttl: 60,
-						data: ns_ip
-					}], req, res);				
-					break;
-				default:
-					me.send([{ 
-						name: question.name,
-						type: 'A',
-						class: 'IN',
-						ttl: 60,
-						data: null
-					}], req, res);					
-			}
-			*/
 		};	
 	};
 	module.exports = obj;
