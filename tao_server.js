2db55b6c9ef13e69c5d8636ebb54566032ac7507
b2blosangeles <b2blosangeles@gmail.com>
2019-07-14 07:26:51 -0700

    Update tao_server.js

--- tao_server.js
+++ tao_server.js
@@ -70,6 +70,14 @@ app.post(/(.+)$/i, function (req, res) {
 	R.load();
 });
 
+function _getServerIP() {
+    var ifaces = require('os').networkInterfaces(), address=[];
+    for (var dev in ifaces) {
+	var v =  ifaces[dev].filter((details) => details.family === 'IPv4' && details.internal === false);
+	for (var i=0; i < v.length; i++) address[address.length] = v[i].address;
+    }
+    return address;
+};	
 
 var server = require('http').createServer(app);
 server.listen(port, function() {
