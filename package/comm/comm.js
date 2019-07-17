b97c41a50e0057063ab01b276c37832055b08e5e
b2blosangeles <b2blosangeles@gmail.com>
2019-07-17 08:40:34 -0700

    Update comm.js

--- package/comm/comm.js
+++ package/comm/comm.js
@@ -1,7 +1,15 @@
 (function () { 
 	var obj =  function () {
+		this._getServerIP = function () {
+		    var ifaces = require('os').networkInterfaces(), address=[];
+		    for (var dev in ifaces) {
+			var v =  ifaces[dev].filter((details) => details.family === 'IPv4' && details.internal === false);
+			for (var i=0; i < v.length; i++) address[address.length] = v[i].address;
+		    }
+		    return address;
+		};
 		this.uuid = function() {
-			return 'UUID'
+			return this._getServerIP()
 		};
 	};
 	module.exports = obj;
