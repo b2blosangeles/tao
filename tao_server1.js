6dc11a5c950cc8373f5a0a114fee6e689ab454fa
b2blosangeles <b2blosangeles@gmail.com>
2019-07-24 14:48:32 -0700

    Update tao_server1.js

--- tao_server1.js
+++ tao_server1.js
@@ -5,7 +5,7 @@ compression = require('./package/compression/node_modules/compression'),
 tls = require('tls'),  
 app			= express(),
 expireTime	= 604800000,
-port 		= 10800;
+port 		= 81;
 
 var LOG = require(__dirname + '/package/log/log.js');
 var log = new LOG();		
