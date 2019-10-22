38f37c7c75a7a19e6eb76a910a26f2036f5c2099
b2blosangeles <b2blosangeles@gmail.com>
2019-10-22 10:45:18 -0700

    Create commandShell.js

---
+++ package/commandShell/commandShell.js
@@ -0,0 +1,53 @@
+(function () {
+      var obj =  function (TAO) {
+            let me = this;
+            
+            me.exec = function(cmd, cbk, timeout) {
+                var { spawn } = TAO.require('child_process');
+                var cmda = cmd.split(/[\s]+/), retStr = {error : []}, normalClosed = false, resultData = '';
+                var ps = spawn(cmda.shift(), cmda, {detached: true});
+                ps.stdout.setEncoding('utf8')
+
+                ps.stdout.on('data', (data) => {
+                    resultData  = data;
+                });
+
+                ps.stderr.on('data', (data) => {
+                      resultData  +=  data;
+                });
+
+                ps.on('error', (code) => {
+                  retStr.error.push(`ps error: ${data}`);
+                });
+                  
+                ps.on('close', (code) => {
+                    normalClosed= true
+                    if (code !== 0) {
+                        retStr.error.push(`ps process exited with code ${code}`);
+                    }
+                    retStr.data = {};
+                    resultData = resultData.replace(/^\s+|\s+$/gm,'')
+                    try {
+                          retStr.data = JSON.parse(resultData);
+                    } catch (e) {
+                          retStr.data = resultData;
+                    }
+                    cbk(resultData);
+                });
+
+                setTimeout(function() {
+                      if (!normalClosed) {
+                          //    ps.kill();
+                          process.kill(-ps.pid);
+                          retStr.error.push('shell command timeout!');
+                          cbk(retStr);
+                      }
+                }, (!timeout) ? 8000 :  timeout)
+            }
+      };
+
+      if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+            module.exports = obj;
+      } 
+
+})();
